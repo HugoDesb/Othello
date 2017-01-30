62d89dba1dfe4b80487ae0f17efa6aa6f3e803d2
HugoDesb <hugo.desbiolles@gmail.com>
2017-01-30 18:00:49 +0100

    Ajout commentaires + abstraction des fonction minimax

--- ia.c
+++ ia.c
@@ -23,8 +23,8 @@
 #define TAILLE 8
 #define NBCASES 64
 
-int minimax_ia (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff);
-int minimax_adv (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff);
+int minimax_ia (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff,char player);
+int minimax_adv (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff,char player);
 int score(char Player_color,char othellier[TAILLE][TAILLE], int diff);
 
 int coeff_othellier[TAILLE][TAILLE] = {{ 500,-150,30,10,10,30,-150, 500},
@@ -36,130 +36,126 @@ int coeff_othellier[TAILLE][TAILLE] = {{ 500,-150,30,10,10,30,-150, 500},
                                        {-150,-250, 0, 0, 0, 0,-250,-150},
                                        { 500,-150,30,10,10,30,-150, 500}};
 
-int minimax_adv(int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff){
-	int i;
-	if (depth == 0)
-		return score('N',othellier,diff);
-    
-	int bestScore;
-  	int move;
+/**
+ * Fonction MiniMax qui renvoie le meilleur coup a jouer (si depth = 0 ) ou le meilleur score 
+ * Appelle la récusrion avec minimax_ia
+ * depth DOIT être impair
+ */
+int minimax_adv(int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff,char player){
+	int i , bestScore , move,score;
+	char opponent;
+	char copie_othellier[TAILLE][TAILLE];
 	tpl coups_jouable;
 
+	//Condition d'arrêt de la récursion
+	if (depth == 0)
+		return score(player,othellier,diff);
+    
+	// Le bestScore est min (donc initialisation au maximum)
 	bestScore = +10000;
-	coups_jouable=coup_jouable('N',othellier);
+	coups_jouable=coup_jouable(player,othellier);
 
+	//Parcours de tous les coups possibles 
 	while(coups_jouable!=0){
-
-		char nothellier[TAILLE][TAILLE];
+		//On fait une copie de l'othellier 
+		//(pour ne pas modifier l'état a partir duquel on explore)
 		for(i=0; i<NBCASES; i++){
-			nothellier[i/TAILLE][i%TAILLE] = othellier[i/TAILLE][i%TAILLE];
+			copie_othellier[i/TAILLE][i%TAILLE] = othellier[i/TAILLE][i%TAILLE];
 		}
+		// Choix d'un coup
 		move=tete_liste(coups_jouable);
-
+		// pour la suite de la boucle 
 		coups_jouable = queue_liste(coups_jouable);
-		//nouveau othellier 
-		change_othellier(move,'N',nothellier);
-		
-		int score = minimax_ia (depth - 1,maxdepth,nothellier,diff);
+		//nouveau othellier avec le coup choisi
+		change_othellier(move,player,copie_othellier);
 
-		//free othellier
+		//calcul du best score en utilisant la récusrion
+		// coté adversaire
+		opponent = getOpponent(player);
+		score = minimax_ia (depth - 1,maxdepth,copie_othellier,diff,opponent);
+
+		//Pour le minmax de l'adversaire, on récupère le score min 
 		if (score < bestScore) {
 				bestScore = score;
-				//bestMove = move ;
 		}
 	}
+	//Dans le cadre de la récursion, on renvoie le score minimum
 	return bestScore ;	
 }
-int minimax_ia (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff)
+
+/**
+ * Fonction MiniMax qui renvoie le meilleur coup a jouer (si depth = 0 ) ou le meilleur score 
+ * Appelle la récusrion avec minimax_adv
+ * Entrée de la fonction (fonction initiale)
+ * depth DOIT être pair
+ */
+int minimax_ia (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff,char player)
 {
-	int i;
-	if (depth == 0)
-		return score('B',othellier, diff);
-    
-	int bestScore;
-	int bestMove;
-  	int move;
+	int i , bestScore , bestMove , move,score ;
+	char opponent;
+	char copie_othellier[TAILLE][TAILLE];
 	tpl coups_jouable;
 
+	//Condition d'arrêt de la récursion
+	if (depth == 0)
+		return score(player,othellier, diff);
+    
+	
+	// Le bestScore est max (donc initialisation au minimum)
 	bestScore = -10000;
-	coups_jouable=coup_jouable('B',othellier);
+	coups_jouable=coup_jouable(player,othellier);
 		
+	//Parcours de tous les coups possibles 
 	while(!est_vide(coups_jouable)){
-		//new othellier (copy)
-		char nothellier[TAILLE][TAILLE];
+		//On fait une copie de l'othellier 
+		//(pour ne pas modifier l'état a partir duquel on explore)
 		for(i=0; i<NBCASES; i++){
-			nothellier[i/TAILLE][i%TAILLE] = othellier[i/TAILLE][i%TAILLE];
+			copie_othellier[i/TAILLE][i%TAILLE] = othellier[i/TAILLE][i%TAILLE];
 		}
-		//move on that 
+
+		// Choix d'un coup
 		move=tete_liste(coups_jouable);
+		// pour la suite de la boucle 
 		coups_jouable = queue_liste(coups_jouable);
-		//nouveau othellier
-		change_othellier(move,'B',nothellier);
-		int score = minimax_adv(depth - 1,maxdepth,nothellier,diff);
-    //if (depth==maxdepth){printf("%d:%d\n",score,bestScore);printf("mm : %d,%d\n",move/TAILLE,move%TAILLE);}
-		//free othellier
+		//nouveau othellier avec le coup choisi
+		change_othellier(move,player,copie_othellier);
+
+		//calcul du best score en utilisant la récusrion
+		// coté adversaire
+		opponent = getOpponent(player);
+		score = minimax_adv(depth - 1,maxdepth,copie_othellier,diff,opponent );
+		//Pour le minmax du joueur, on récupère le score max 
+		// avec son coup associé
 		if (score > bestScore) {
 			bestScore = score;
 			bestMove = move ;
 		}
 	}
 
+	//On retourne le coup choisi
    	if (depth==maxdepth){
-       printf("SCOOOOOOOOORE : %d\n",bestScore);
-       printf("MOOOOOOOOOVE : %d,%d\n",bestMove/TAILLE,bestMove%TAILLE);
         return bestMove;
     }  
 
-   	
+   	//Dans le cadre de la récursion, on renvoie le score maximum
 	return bestScore ;
 }
 
+/**
+ * Calcul du score pour la couleur donnée et la difficulté 
+ * (si = 2 ou 3, prise en compte des coefficients)
+ */
 int score(char Player_color,char othellier[TAILLE][TAILLE], int diff){
 	int score=0,i;
   
 	for (i=0;i<NBCASES;i++){
 		if (othellier[i/TAILLE][i%TAILLE]==Player_color){
-      if(diff==3){
-        score+=coeff_othellier[i/TAILLE][i%TAILLE];
-      }else{
-        score++;
-      }		
-	  }
-  }
-
-
-  //////------------------affiche othellier------------------------------------------------------
-  #define NORMALCOLOR  "\x1B[0m"
-#define REDCOLOR  "\x1B[31m"
-#define YELLOWCOLOR  "\x1B[33m"
-#define BLUECOLOR  "\x1B[34m"
-
-  printf("Score : %d\n",score);
-	//Affichage des num en haut
-	for(i=0; i<TAILLE;i++){
-		printf(" %c ",i+65);
-	}
-	
-	for(i=0; i<NBCASES;i++){
-		if(i%(TAILLE) == 0){
-			if (i!=0){
-				printf("%d",i/(TAILLE)-1);
-			}
-			printf("\n");		
-		}
-
-		
-		//Affichage en couleur d'un blanc(bleu) ou noir(rouge)
-		if(othellier[i/TAILLE][i%TAILLE]=='N'){
-			printf(" %sN%s ",BLUECOLOR,NORMALCOLOR);
-		}else if(othellier[i/TAILLE][i%TAILLE]=='B'){
-			printf(" %sB%s ",REDCOLOR,NORMALCOLOR);			
-		}else{
-				printf(" V ");
+			if(diff==3 || diff == 2){
+				score+=coeff_othellier[i/TAILLE][i%TAILLE];
+  	    	}else{
+  	    		score++;
+			}		
 		}
 	}
-	printf("%d\n",TAILLE-1);
-
-
   return score;
 }
--- ia.h
+++ ia.h
@@ -9,6 +9,24 @@
 #define TAILLE 8
 #define NBCASES 64
 
-int minimax_ia (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff);
-int minimax_adv (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff);
+
+/**
+ * Fonction MiniMax qui renvoie le meilleur coup a jouer (si depth = 0 ) ou le meilleur score 
+ * Appelle la récusrion avec minimax_adv
+ * Entrée de la fonction (fonction initiale)
+ * depth DOIT être pair
+ */
+int minimax_ia (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff,char player);
+
+/**
+ * Fonction MiniMax qui renvoie le meilleur coup a jouer (si depth = 0 ) ou le meilleur score 
+ * Appelle la récusrion avec minimax_ia
+ * depth DOIT être impair
+ */
+int minimax_adv (int depth,int maxdepth,char othellier[TAILLE][TAILLE], int diff,char player);
+
+/**
+ * Calcul du score pour la couleur donnée et la difficulté 
+ * (si = 2 ou 3, prise en compte des coefficients)
+ */
 int score(char Player_color,char othellier[TAILLE][TAILLE], int diff);
